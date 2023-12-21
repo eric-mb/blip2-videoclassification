ac680d3ebac8bd3c83bb89c7052a1250fdaf24dd
eric-mb <ericmb@tuta.io>
2023-12-21 13:22:13 +0100

    added requirements and 4 bit quantization

--- inference.py
+++ inference.py
@@ -21,12 +21,32 @@ class BLIP2:
         repetition_penalty=1.5,
         length_penalty=1.0,
         temperature=1,
+        precision=4,
     ):
-        self._model = InstructBlipForConditionalGeneration.from_pretrained(model)
-        self._processor = InstructBlipProcessor.from_pretrained(processor)
-
         self._device = "cuda" if torch.cuda.is_available() else "cpu"
-        self._model.to(self._device)
+
+        if precision == 4:
+            self._dtype = torch.bfloat16
+            self._model = InstructBlipForConditionalGeneration.from_pretrained(
+                model,
+                device_map="auto",
+                load_in_4bit=True,
+                torch_dtype=self._dtype,
+            )
+
+        elif precision == 8:
+            self._dtype = torch.bfloat16
+            self._model = InstructBlipForConditionalGeneration.from_pretrained(
+                model,
+                device_map="auto",
+                load_in_8bit=True,
+                torch_dtype=self._dtype,
+            )
+        else:  # 32bit
+            self._model = InstructBlipForConditionalGeneration.from_pretrained(model)
+            self._model.to(self._device)
+
+        self._processor = InstructBlipProcessor.from_pretrained(processor)
 
         self._params = {
             "do_sample": do_sample,
@@ -40,7 +60,7 @@ class BLIP2:
         }
 
     def get_response(self, image, query):
-        inputs = self._processor(images=image, text=query, return_tensors="pt").to(self._device)
+        inputs = self._processor(images=image, text=query, return_tensors="pt").to(self._device, dtype=self._dtype)
         outputs = self._model.generate(**inputs, **self._params)
         return self._processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
 
--- infererence_interactive.py
+++ infererence_interactive.py
@@ -1,4 +1,5 @@
 # %%
+import imageio
 from inference import BLIP2
 from video_decoder import VideoDecoder
 import matplotlib.pyplot as plt
@@ -11,11 +12,20 @@ vd = VideoDecoder(path="../fakenarratives/temp/compacttv_2022_01_10_kX5DF1JDnqg.
 vd_iter = iter(vd)
 query = "Is the photo taken in a news studio?"
 
-# %%
-frame = next(vd_iter)
-response = model.get_response(image=frame["frame"], query=query)
+# %% Get resonsonses for video frames
+frame = next(vd_iter)["frame"]
+response = model.get_response(image=frame, query=query)
 
-plt.imshow(frame["frame"])
+plt.imshow(frame)
+print(f"Query: {query}")
+print(f"Response: {response}")
+
+# %% Get resonsonses for an image
+
+frame = imageio.imread("temp/screenshots_fakenarratives/compacttv_protest_anchor.png")[:, :, 0:3]
+response = model.get_response(image=frame, query=query)
+
+plt.imshow(frame)
 print(f"Query: {query}")
 print(f"Response: {response}")
 
